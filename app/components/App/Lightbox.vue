c6150fbeea575fe133b38eb84ad2031eda8a8fa1
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-07-04 20:32:19 -0500

    Adds initial attempt at lightbox component

--- app/components/App/Lightbox.vue
+++ app/components/App/Lightbox.vue
@@ -14,14 +14,43 @@ const selectedImage = ref<number>(1);
 </script>
 
 <template>
-  <div class="relative grid place-items-center justify-center gap-6">
-    <img src="/icon-close.svg" alt="Close" class="absolute right-0 top-0 cursor-pointer" />
-    <img
-      :src="`/image-product-${selectedImage}.jpg`"
-      :alt="`Fall limited edition sneakers - image ${selectedImage}`"
-      class="h-full w-full rounded-2xl object-cover"
-    />
-    <div class="grid grid-cols-4 gap-6 px-10">
+  <div class="bg-theme-grayish-blue flex h-screen flex-col items-center justify-center gap-10">
+    <div class="relative">
+      <svg
+        class="text-theme-white hover:text-theme-orange absolute -top-12 right-0 size-6 cursor-pointer fill-current transition-colors delay-200"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 14 15"
+      >
+        <path
+          d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+          fill-rule="evenodd"
+        />
+      </svg>
+      <img
+        :src="`/image-product-${selectedImage}.jpg`"
+        :alt="`Fall limited edition sneakers - image ${selectedImage}`"
+        class="size-128 rounded-2xl object-cover"
+      />
+
+      <button
+        class="hover:text-theme-orange absolute -right-6 top-1/2 flex size-12 -translate-y-1/2 items-center justify-center rounded-full bg-white shadow-lg transition-colors delay-200"
+        @click="selectedImage = selectedImage === 4 ? 1 : selectedImage + 1"
+      >
+        <svg class="size-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 18">
+          <path d="m2 1 8 8-8 8" stroke-width="2" fill="none" fill-rule="evenodd" />
+        </svg>
+      </button>
+
+      <button
+        class="hover:text-theme-orange absolute -left-6 top-1/2 flex size-12 -translate-y-1/2 items-center justify-center rounded-full bg-white shadow-lg transition-colors delay-200"
+        @click="selectedImage = selectedImage === 1 ? 4 : selectedImage - 1"
+      >
+        <svg class="size-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 18">
+          <path d="M11 1 3 9l8 8" stroke-width="2" fill="none" fill-rule="evenodd" />
+        </svg>
+      </button>
+    </div>
+    <div class="grid grid-cols-4 gap-6 px-8">
       <div
         v-for="image in imageList"
         :key="image.id"
@@ -35,7 +64,7 @@ const selectedImage = ref<number>(1);
           :alt="image.alt"
           :class="[
             selectedImage === image.id ? 'rounded opacity-30' : 'rounded-xl hover:opacity-30',
-            'transition-opacity duration-200',
+            'size-20 transition-opacity duration-200',
           ]"
           @click="selectedImage = image.id"
         />
---
+++ app/pages/test.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+});
+import productDetails from "@@/data/products.json";
+
+const productId = 1;
+const product = computed(() => productDetails.find((p) => p.id === productId)) as ComputedRef<
+  Product | undefined
+>;
+const images = computed(() => product.value?.images ?? []);
+</script>
+
+<template>
+  <AppLightbox :imageList="images" />
+</template>
