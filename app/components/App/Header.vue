b542b7d330c44164d18d56f3fd2a6d04daa246e3
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-07-11 22:28:46 -0500

    Adds ARIA attributes to improve accessibility

--- app/components/App/Header.vue
+++ app/components/App/Header.vue
@@ -22,14 +22,21 @@ function toggleMobileMenu() {
 
 <template>
   <header
+    role="banner"
     class="border-theme-grayish-blue/30 relative flex items-center justify-between px-8 py-4 md:border-b md:px-0 md:py-7"
   >
     <div class="flex items-center gap-14">
-      <button aria-label="Open menu" class="md:hidden" @click="toggleMobileMenu">
+      <button
+        aria-label="Open menu"
+        :aria-expanded="showMobileMenu"
+        aria-controls="mobile-menu"
+        class="md:hidden"
+        @click="toggleMobileMenu"
+      >
         <img src="/icon-menu.svg" alt="Menu" class="size-4" />
       </button>
-      <img src="/logo.svg" alt="Sneakers Logo" class="h-5" />
-      <nav class="hidden md:block">
+      <img src="/logo.svg" alt="Sneakers Logo" class="h-5" aria-label="Sneakers Logo" />
+      <nav class="hidden md:block" role="navigation">
         <ul class="text-theme-dark-grayish-blue flex items-center gap-8">
           <li v-for="item in navItems" :key="item.label" class="group relative cursor-pointer">
             <NuxtLink
@@ -46,7 +53,14 @@ function toggleMobileMenu() {
       </nav>
     </div>
     <div class="relative flex items-center gap-10">
-      <button aria-label="View cart" class="relative cursor-pointer" @click="toggleCart">
+      <button
+        aria-label="View cart"
+        aria-haspopup="true"
+        :aria-expanded="showCart"
+        aria-controls="cart-panel"
+        class="relative cursor-pointer"
+        @click="toggleCart"
+      >
         <img src="/icon-cart.svg" alt="Cart" />
         <span
           v-if="cartCounter > 0"
@@ -60,10 +74,24 @@ function toggleMobileMenu() {
         alt="User avatar"
         class="hover:border-theme-orange md:hover:border-theme-orange size-7 cursor-pointer rounded-full border-2 border-transparent transition-all duration-200 hover:scale-105 md:size-14 md:hover:scale-110"
       />
-      <div v-if="showCart" class="absolute -right-20 top-[calc(100%+10px)]">
+      <div
+        v-if="showCart"
+        class="absolute -right-20 top-[calc(100%+10px)]"
+        id="cart-panel"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="cart-title"
+      >
         <AppCartContents @close="toggleCart" />
       </div>
-      <div v-if="showMobileMenu" class="fixed inset-0 bg-black/70">
+      <div
+        v-if="showMobileMenu"
+        class="fixed inset-0 bg-black/70"
+        id="mobile-menu"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="mobile-menu-title"
+      >
         <AppMobileMenu @close="toggleMobileMenu" />
       </div>
     </div>
