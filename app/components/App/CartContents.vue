9f4bf768f263222de11b65402bdc1de2e8b53cca
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-07-11 22:03:04 -0500

    Adds ARIA attributes to improve accessibility

--- app/components/App/CartContents.vue
+++ app/components/App/CartContents.vue
@@ -26,16 +26,17 @@ const emit = defineEmits(["close"]);
 </script>
 
 <template>
-  <div class="w-[360px] rounded-xl bg-white pb-8 pt-4 shadow-xl">
+  <div class="w-[360px] rounded-xl bg-white pb-8 pt-4 shadow-xl" role="dialog" aria-label="Shopping Cart" tabindex="-1">
     <h2 class="text-theme-very-dark-blue mb-7 px-6 font-bold">Cart</h2>
     <hr class="border-theme-grayish-blue/30 mb-6" />
     <div
       v-if="cartContents.length === 0"
       class="text-theme-dark-grayish-blue px-6 py-16 text-center font-semibold"
+      aria-label="Your shopping cart is currently empty"
     >
       Your cart is empty.
     </div>
-    <div v-else class="flex flex-col gap-6 px-5">
+    <div v-else class="flex flex-col gap-6 px-5" aria-live="polite">
       <div class="flex items-center gap-4" v-for="item in cartContents" :key="item.id">
         <img
           :src="product(item.id)?.images[0]?.thumbnail"
@@ -47,17 +48,22 @@ const emit = defineEmits(["close"]);
           <div class="text-theme-grayish-blue flex items-center gap-1">
             <span class="text-theme-dark-grayish-blue">${{ discountPrice(item) }}</span>
             <span class="text-theme-dark-grayish-blue">x</span>
-            <span class="text-theme-dark-grayish-blue">{{ item.quantity }}</span>
+            <span class="text-theme-dark-grayish-blue" aria-label="Quantity: {{ item.quantity }}">{{ item.quantity }}</span>
             <span class="text-theme-very-dark-blue ml-2 font-bold">${{ totalPrice(item) }}</span>
           </div>
         </div>
-        <button class="cursor-pointer p-1 transition hover:opacity-70" @click="emptyCart">
+        <button 
+          class="cursor-pointer p-1 transition hover:opacity-70" 
+          @click="emptyCart"
+          aria-label="Remove item from cart"
+        >
           <img src="/icon-delete.svg" alt="Remove from cart" />
         </button>
       </div>
       <button
         class="bg-theme-orange text-shadow-theme-very-dark-blue w-full rounded-lg py-4 font-bold transition hover:bg-orange-400"
         @click="emptyCart"
+        aria-label="Proceed to checkout"
       >
         Checkout
       </button>
